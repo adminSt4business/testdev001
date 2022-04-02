42d92a304ca872265861f8599c6214b132283aaa
Javier Prieto <jprieto@st4business.com>
2022-04-02 15:40:19 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,22 @@
+# _*_ coding: utf-8 _*_
+{
+    'name': 'Odoo Acamedy',
+    'Sumary': """Academy app to manage Training""",
+    'description': """
+       Academy Module to manage Training
+       + Courses
+       + Sessions
+       + Atendees
+    """,
+    'author': 'JPrieto',
+    'website': 'https://www.st4business.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data':[
+        
+    ],
+    'demo':[
+        
+    ],
+}
