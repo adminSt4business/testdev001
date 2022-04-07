ed474db542930cc9706d2ed60a4e07f260cf0166
J.Prieto <jprieto@st4business.com>
2022-04-07 01:04:10 +0000

    [UPD-3]Estate Module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 {
-    'name': 'Real Estate',
+    'name': 'My Real Estate',
     'version': '0.1',
     'category': 'sales/realty',
     'sumary': 'Monitoring of real estate sales processes'    
