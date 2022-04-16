bd48b4bdbdfd33edd27560fa97fa6b3b34dfc13a
J.Prieto <jprieto@gmail.com>
2022-04-16 21:56:17 +0000

    [UPD-4.4.3] Modulo Base

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -15,8 +15,6 @@
     'version': '0.1',
     'depends': ['base'],
     'data':[
-        'security/academy_security.xml',
-        'security/ir.model.access.csv'
     ],
     'demo':[
         'demo/academy_demo.xml'
