150dd10150b216e58a435d98617f1f4a36d96991
J.Prieto <jprieto@gmail.com>
2022-04-17 21:30:27 +0000

    [UPD-3.0.1] Estate Module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,18 @@
 # _*_ coding: utf-8 _*_
 {
     'name': 'My Real Estate',
-    'version': '0.1',
-    'category': 'sales/realty',
     'sumary': 'Monitoring of real estate sales processes'    
+    'category': 'sales/realty',
+    'version': '0.1',
+    'license': 'GNU LGPL v3',    'depends': ['base'],
+    'author': "Javier Prieto",
+    'website': 'https://www.st4business.com',
+    #data files always Loaded at intallation
+    'data': [
+        
+    ],
+    #data files containing optionally Loaded demostration data
+    'demo': [
+        
+    ],
 }
