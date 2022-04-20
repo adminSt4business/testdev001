724e3e5779384d340d0dd86db7169443be505e3f
J.Prieto <jprieto@gmail.com>
2022-04-20 14:02:01 +0000

    [UPD-5.0.2] Modulo Base

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -6,12 +6,13 @@ class Course(models.Model):
     _description = 'Course Info'
     
     name = fields.Char(string='Title', required=True)
-    description = fields.Text(string='Description')
+    description = fields.Text(string='Description', required=True)
     level = fields.Selection(string='Level',
                              selection=[('beginer','Beginer'),
                                         ('intermediate','Intermediate'),
                                         ('advanced','Advanced')
                                        ],
-                             copy=False
+                             copy=False, 
+                             required=True
                             )
     active = fields.Boolean(string='Active', default=True)
